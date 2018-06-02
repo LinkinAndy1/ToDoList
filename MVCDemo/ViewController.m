4f3cd3fd5f418eace3f0b46a9b6ed48358a1f6d4
yu <yu@yudeimac-8.local>
2018-06-02 15:01:29 +0800

    Initial Commit

---
+++ MVCDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MVCDemo
+//
+//  Created by yu on 2018/6/2.
+//  Copyright © 2018年 yu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
